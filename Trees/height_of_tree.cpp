9bef49c31ba0d2306a45cd3060147af6c73bd871
Rakshank Verma <rakshankverma130250@gmail.com>
2023-08-08 21:00:39 +0530

    Day9(Tree)

---
+++ Trees/height_of_tree.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+
+
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        return 1+max(maxDepth(root->left), maxDepth(root->right));        
+    }
+};
